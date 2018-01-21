7b66cd5213f4eb4b2659578652af3c0482cb9cef
SSASoft <nvd.mcs@gmail.com>
2018-01-21 22:02:04 +0500

    ViewController
    #Method IB Action
    # added comment line

--- AlertViewDemo/AlertViewDemo/ViewController.swift
+++ AlertViewDemo/AlertViewDemo/ViewController.swift
@@ -35,8 +35,8 @@ class ViewController: UIViewController {
     }
     
     @IBAction func alertActionPressed(_ sender: UIButton) {
-          alertView(title: "Do You like tea", message: "YES OR NO") // ali bajwa
-          alertView(title: "Do You like Tea", message: "YES OR NO")
+          alertView(title: "Do You like tea", message: "YES OR NO") // ali Naveed
+          // this is more improve version of commit style
     }
     
 
