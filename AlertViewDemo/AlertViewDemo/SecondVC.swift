08ac7eb581d4d27b830829120a657394b73c979e
Ali Bajwa <bajwaali230@gmail.com>
2018-01-21 23:32:19 +0500

    Response alertview Added

---
+++ AlertViewDemo/AlertViewDemo/SecondVC.swift
@@ -0,0 +1,19 @@
+//
+//  SecondVC.swift
+//  AlertViewDemo
+//
+//  Created by Ali Bajwa on 21/01/2018.
+//  Copyright © 2018 SSASoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- AlertViewDemo/AlertViewDemo/ViewController.swift
+++ AlertViewDemo/AlertViewDemo/ViewController.swift
@@ -21,23 +21,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func alertView(title: String , message: String) {
+    func simpleAlertView(title: String , message: String) {
         
         let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let yes = UIAlertAction(title: "YES", style: .cancel, handler: nil)
-        let no = UIAlertAction(title: "No", style: .default, handler: nil)
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
         //This is New lime
-        alertController.addAction(no)
-        alertController.addAction(yes)
+        alertController.addAction(ok)
+   //     alertController.addAction(yes)
         
         present(alertController, animated: true, completion: nil)
     }
     
+    func responseAlertView(title: String , message: String) {
+        
+        let alertCotroller = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let yes = UIAlertAction(title: "YES", style: .default) { (responseAction) in
+            
+            self.performSegue(withIdentifier: "SecondVC", sender: self)
+        }
+        let no = UIAlertAction(title: "NO", style: .default, handler: nil)
+        alertCotroller.addAction(yes)
+        alertCotroller.addAction(no)
+        present(alertCotroller, animated: true, completion: nil)
+    }
+    
     @IBAction func alertActionPressed(_ sender: UIButton) {
-          alertView(title: "Do You like tea", message: "YES OR NO") // ali bajwa
+        
+          simpleAlertView(title: "2 + 2 = 4", message: "") // ali bajwa
 
   }
+  
+    @IBAction func responseAlertPressed(_ sender: UIButton) {
+        
+      responseAlertView(title: "Do you like pizza", message: "YES or No")
+       
+    }
+    
+    
     
 }
 
